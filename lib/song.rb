881549d0b16ade3de99e5f531e3bf468bede7203
Charles <charlesangeloplai@gmail.com>
2020-11-19 17:08:01 -0500

    commit tree 78b0f58a55eb263e64532fae341a5afec186aca1

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -7,7 +9,72 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |person| person.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      object = []
+
+      @@all.each do |object_song|
+        if object_song.name == name
+          object = object_song
+        end
+      end
+
+      object
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(file_name)
+    file_name = parse_file(file_name)
+    self.create_by_name_and_artist(file_name.last, file_name.first)
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.new_from_filename(file_name)
+    song.save
+  end
+
+  def self.create_by_name_and_artist(name, artist)
+    song = self.create_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.parse_file(file_name)
+    file_name = file_name.gsub(".mp3","").split(" - ")
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
